cae51675a5a5bc4b94b4a92eb81173024e46fbbd
Amber Doyle <adpd1g23@soton.ac.uk>
2026-03-30 15:09:30 +0100

    Subsetting to removed the MYOD1 cells and checking that the number of MYOD1 cells by subsetting matched those that were removed which is a yes.

--- Differential FN FP-RMS NK ligs 30.03.26.R
+++ Differential FN FP-RMS NK ligs 30.03.26.R
@@ -14,3 +14,11 @@ RMS$newfusion <- mapvalues(x= RMS$fusion, from= c("PAX3::FOXO1", "PAX7::FOXO1"),
                            to= c("FP-RMS", "FP-RMS"))
 unique(RMS$newfusion)
 
+RMS.noMYOD1 <- subset(RMS, subset = newfusion != "MYOD1")
+unique(RMS.noMYOD1$newfusion)
+length(RMS$newfusion) #107523
+length(RMS.noMYOD1$newfusion) #106023
+
+#Check that the same number of MYOD1 has been removed from above
+RMS.MYOD1 <- subset(RMS, subset = newfusion == "MYOD1")
+length(RMS.MYOD1$newfusion) #1500 so the above did remove all the MYOD1. 
