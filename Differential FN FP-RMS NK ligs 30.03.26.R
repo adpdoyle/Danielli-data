b8d44e7c4c651c971101fde182ce06653e86cd78
Amber Doyle <adpd1g23@soton.ac.uk>
2026-03-30 17:55:39 +0100

    Aggregated the RMS.noMYOD1 Seurat object based on "id" and "newfusion" metadata. This created a list and then took matrix out of list. The matrix object is dgcmatrix.

--- Differential FN FP-RMS NK ligs 30.03.26.R
+++ Differential FN FP-RMS NK ligs 30.03.26.R
@@ -22,3 +22,17 @@ length(RMS.noMYOD1$newfusion) #106023
 #Check that the same number of MYOD1 has been removed from above
 RMS.MYOD1 <- subset(RMS, subset = newfusion == "MYOD1")
 length(RMS.MYOD1$newfusion) #1500 so the above did remove all the MYOD1. 
+
+#Set the default assay to RNA not the integrated which is batch corrected. 
+DefaultAssay(RMS.noMYOD1) <- "RNA"
+
+unique(RMS.noMYOD1$PatientID)
+unique(RMS.noMYOD1$id)
+
+#Aggregate the counts based on the id of the sample and the newfusion status.
+Aggcounts <- AggregateExpression(RMS.noMYOD1, assays= "RNA",
+                    group.by = c("id", "newfusion"),
+                    normalization.method = "LogNormalize")
+
+#get the matrix from the list, it is under RNA as you set assays to RNA. 
+Aggcountsmat <- Aggcounts$RNA
